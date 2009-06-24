d485c943580a459cfeea21eda59a416075d1e000
jjl <jjl@jameslaver.com>
2009-06-24 22:42:34 +0000

    Docs update for example module

--- lib/Net/Twitter/Antispam/Plugin/Example.pm
+++ lib/Net/Twitter/Antispam/Plugin/Example.pm
@@ -19,3 +19,50 @@ sub is_user_spammy {
     #As far as we're aware, we have no clue whether they're a spammer
     return 50;
 }
+
+1;
+__END__
+
+=head1 NAME
+
+Net::Twitter::Antispam::Plugin::Example - an example plugin for Net::Twitter::Antispam that blocks an old twitter tactic
+
+=head1 DESCRIPTION
+
+To implement a plugin, you need to decide whether your plugin tests for a user being a spammer or an individual message being spamlike.
+
+There are two predefined method names you can user: C<is_user_spammy> and C<is_message_spammy>.
+
+You need only implement one of these, this module only implements C<is_user_spammy>
+
+Each function should return a percentage value of spam likelihood with 100% being 'definitely spam', 0% being 'definitely not spam' and 50% being 'no clue'.
+
+The results from every module being used will be averaged to guess whether it is spam or not.
+
+=head1 INTERFACE
+
+=head1 is_user_spammy
+
+receives three arguments: a Net::Twitter::Antispam, a Net::Twitter, and a hashref representing the user that was returned from Net::Twitter.
+
+It is expected to return a percentage spam likelihood.
+
+=head2 is_message_spammy
+
+receives three arguments: a Net::Twitter::Antispam, a Net::Twitter, and a copy of the message
+
+It is expected to return a percentage spam likelihood.
+
+=head1 FUNCTIONS
+
+=head2 is_user_spammy :: (Net::Twitter::Antispam, Net::Twitter, HashRef) => Int
+
+This is a method called automatically by Net::Twitter::Antispam. In this case it combats an old spamming tactic.
+
+=head1 SEE ALSO
+
+L<Net::Twitter::Antispam> - The anti-spam system for twitter
+L<Net::Twitter> - You'll be passed one of these implementing
+=cut
+
+
